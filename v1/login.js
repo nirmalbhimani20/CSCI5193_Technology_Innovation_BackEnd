4dcf015bf5f39fbbc2f1e0934b2fd33f0e3fa4a8
Tejaswi Rameshkumar Chaudhary <tj754396@dal.ca>
2021-11-28 15:28:03 -0400

    Modified login-backend validation

--- v1/login.js
+++ v1/login.js
@@ -15,21 +15,32 @@ module.exports = {
             res.json({'status': 'False' , 'number': '103', 'Message': 'Password null' });
         }
         else {
-
-            var query1 = "select * from `sql10451610`.`user` where email = '"+email+"' and password = '"+password+"' ";
+            var now = new Date();
+                var query1 = "select * from `sql10451610`.`user` where email = '"+email+"' and password = '"+password+"' ";
 
             conn.query(query1 , (err, result) => {
                 if (err) {
                     res.json({'status': 'False' , 'number': '104', 'Message': 'Try Again' });
                 }
                 else {
-                    res.json({'status':'true' , 'number':'107' ,'Message':'Successfully Login'});   
+                    var query2 = "select * from `csci5193`.`user` where email = '"+email+"' and password = '"+password+"' ";
+                    conn.query(query2 , (err, result1) => {
+                        if (err) {
+                            res.json({'status': 'False' , 'number': '104', 'Message': 'Try Again' });
+                        }
+                        else {
+                            if(result1.length > 0){
+
+                                res.json({'status':'true' , 'number':'107' ,'Message':'Successfully Login' , "abc": 'abc','result': result1[0] });   
+                        
+                            }
+                            else {
+                                res.json({'status': 'False' , 'number': '104', 'Message': 'Try Again' });
+                            }
+                             }
+                    })         
                 }
-            })
-            
+            })   
         }
-
-
-    }
-    
+    }  
 }
