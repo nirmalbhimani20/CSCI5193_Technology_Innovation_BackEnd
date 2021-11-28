739aaba3f2e634c72e274069befc69c59ac57b85
Tejaswi Rameshkumar Chaudhary <tj754396@dal.ca>
2021-11-28 16:02:44 -0400

    Added login route

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,10 @@
 var express = require('express');
 const router = express.Router();
 const register  = require('../v1/register');
+const login = require('../v1/login');
 
 // app.use('/admin' , require('./user/login'));
 router.post('/user/register' , register.register);
-router.get('/user/test', register.test)
+router.post('/user/login' , login.login );
 
 module.exports = router;
